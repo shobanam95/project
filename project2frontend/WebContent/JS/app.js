4691aa30578f30a613edfc65cd9d4885236600b3
shobanam95 <shobana127@gmail.com>
2017-01-06 21:04:50 +0530

    Add files via upload

--- project2frontend/WebContent/JS/app.js
+++ project2frontend/WebContent/JS/app.js
@@ -4,16 +4,34 @@ app.controller('ctrl',function ($scope,$http,$window,$location)
 {
 var url = "http://"+$window.location.host+"/prjt2backend";	
 
-$scope.add=function()
+$scope.add=function reg()
 {
-	$http.post(url+'/register/'+$scope.username_2).success(function(data)
+	alert ("function");
+	$http.post(url+'/register/'+$scope.username_2+'/'+$scope.email_2+'/'+$scope.password_2+'/'+$scope.confirmpassword_2+'/'+$scope.phno_2).success(function(data)
 {
-		$log.log(data);
-
+		
+alert("controller");
 });
 }
 
+})
 
+
+var app = angular.module('login',[])
+app.controller('logctrl',function ($scope,$http)
+		{
+	$scope.log = function log(){
+		
+		$http.post(url+'/login/'+$scope.username_2+'/'+$scope.password_2).success(function(data)
+	
+				{
+			
+			alert("controller");
+			});
+	
+		
+
+		}
 
 
 })
