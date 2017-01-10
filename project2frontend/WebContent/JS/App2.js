2d86c31587847a68731cf1a49361e9892063ce5d
shobanam95 <shobana127@gmail.com>
2017-01-10 11:57:04 +0530

    Add files via upload

--- project2frontend/WebContent/JS/App2.js
+++ project2frontend/WebContent/JS/App2.js
@@ -1,4 +1,20 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.name = "John Doe";
-});
+var app = angular.module('login',[])
+app.controller('logctrl',function ($scope,$http,$window,$location)
+		{
+	alert("entering");
+	var url1 = "http://"+$window.location.host+"/prjt2backend";	
+	$scope.log = function log(){
+		alert("Controller");
+		$http.post(url1+'/login/'+$scope.username_2+'/'+$scope.password_2).success(function(data)
+	
+				{
+			
+			alert("controller");
+			});
+	
+		
+
+		}
+
+
+})
--- project2frontend/WebContent/JS/app.js
+++ project2frontend/WebContent/JS/app.js
@@ -4,9 +4,11 @@ app.controller('ctrl',function ($scope,$http,$window,$location)
 {
 var url = "http://"+$window.location.host+"/prjt2backend";	
 
-$scope.add=function reg()
+$scope.reg=function reg()
 {
 	alert ("function");
+	
+
 	$http.post(url+'/register/'+$scope.username_2+'/'+$scope.email_2+'/'+$scope.password_2+'/'+$scope.confirmpassword_2+'/'+$scope.phno_2).success(function(data)
 {
 		
@@ -17,21 +19,5 @@ alert("controller");
 })
 
 
-var app = angular.module('login',[])
-app.controller('logctrl',function ($scope,$http)
-		{
-	$scope.log = function log(){
-		
-		$http.post(url+'/login/'+$scope.username_2+'/'+$scope.password_2).success(function(data)
-	
-				{
-			
-			alert("controller");
-			});
-	
-		
-
-		}
 
 
-})
