acff93641511ec84f9206e9f042058fa0ff8acd7
shobanam95 <shobana127@gmail.com>
2017-01-23 10:58:22 +0530

    Add files via upload

--- project2frontend/WebContent/JS/App2.js
+++ project2frontend/WebContent/JS/App2.js
@@ -1,6 +1,8 @@
-var app = angular.module('login',[])
+var app = angular.module('login',['ngRoute'])
+
 app.controller('logctrl',function ($scope,$http,$window,$location)
 		{
+	$scope.user={username_2:'',email_2:'',password_2:'',confirmpassword_2:'',phno_2:''};
 	alert("entering");
 	var url = "http://"+$window.location.host+"/prjt2backend";		
 	$scope.log = function login(){
@@ -19,8 +21,23 @@ app.controller('logctrl',function ($scope,$http,$window,$location)
 
 })
 
-
-
-
-
-
+app.controller('UserController',function($scope,$location,UserService){
+	$scope.user={id:'',username:'',password:'',email:'',role:'',isOnline:'',enabled:''};
+	$scope.message;
+	$scope.submit=function(){
+		console.log('Entering - submit function in usercontroller')
+		UserService.authenticate($scope.user)
+		.then(function(response){
+			if(response.status==401)
+				{
+				  $scope.message="Invalid Username and Password";
+				  $location.path("/login");
+				}
+			else{
+				$scope.user=response.data;
+				$location.path("/home");
+			}
+		})
+	}
+	
+})
